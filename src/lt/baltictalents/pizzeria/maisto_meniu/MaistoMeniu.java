b78b5bcd8d4b92f9cefa074265875c323f8ebfec
Virgilija <virgilija.dvozeckyte@gmail.com>
2018-08-14 15:41:57 +0300

    MaistoMeniu abstrakti klase

--- src/lt/baltictalents/pizzeria/maisto_meniu/MaistoMeniu.java
+++ src/lt/baltictalents/pizzeria/maisto_meniu/MaistoMeniu.java
@@ -1,4 +1,9 @@
 package lt.baltictalents.pizzeria.maisto_meniu;
 
 public abstract class MaistoMeniu {
+
+    abstract void karstiPatiekalai();
+    abstract void saltiPatiekalai();
+    abstract void desertai();
+
 }
