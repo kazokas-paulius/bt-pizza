de7a47c125ab2875d65eea9f3c7d1d9c7907739c
Paulius Kazokas <BTpauliuskazokas@gmail.com>
2018-08-22 12:37:09 +0300

    TextReader upgraded

--- src/lt/baltictalents/pizzeria/read_txt/TxtReader.java
+++ src/lt/baltictalents/pizzeria/read_txt/TxtReader.java
@@ -8,36 +8,19 @@ import java.util.Collections;
 import java.util.List;
 
 public class TxtReader {
+    public static void readMeniuTxt(String fPath) throws IOException{
+        File fTarget = new File(fPath);
+        BufferedReader bfr = new BufferedReader(new FileReader(fTarget));
+        String fContent;
 
-    public static void readFoodTxt() throws IOException {
-        File mFile = new File("resources/Food");
-        BufferedReader mBr = new BufferedReader(new FileReader(mFile));
-        String mContent;
-
-        while ((mContent = mBr.readLine()) != null) {
-            System.out.println(mContent);
-            String[] mName = mContent.split(" ");
+        while ((fContent = bfr.readLine()) != null) {
+            System.out.println(fContent);
+            String[] mName = fContent.split(" ");
             List<String> mNames = Collections.singletonList(mName[1]);
             List<String> mPrices = Collections.singletonList(mName[mName.length - 1]);
             System.out.println(mNames);
             System.out.println(mPrices);
         }
     }
-
-    public static void readDrinksTxt() throws IOException {
-        File gFile = new File("resources/Drinks");
-        BufferedReader gBr = new BufferedReader(new FileReader(gFile));
-        String gContent;
-
-        while ((gContent = gBr.readLine()) != null) {
-            System.out.println(gContent);
-            String[] gName = gContent.split(" ");
-            List<String> gNames = Collections.singletonList(gName[1]);
-            List<String> gPrices = Collections.singletonList(gName[gName.length - 1]);
-            System.out.println(gNames);
-            System.out.println(gPrices);
-        }
-    }
-
 }
 
