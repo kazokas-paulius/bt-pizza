b3d044623f8bc3357f60c34dcc90189870896d9e
Paulius Kazokas <BTpauliuskazokas@gmail.com>
2018-08-23 16:13:02 +0300

    Order Generator

--- src/lt/baltictalents/pizzeria/pizzeria_menus/MenuChoices.java
+++ src/lt/baltictalents/pizzeria/pizzeria_menus/MenuChoices.java
@@ -1,12 +1,10 @@
 package lt.baltictalents.pizzeria.pizzeria_menus;
 
 import lt.baltictalents.pizzeria.Food.MenuItem;
-import lt.baltictalents.pizzeria.PizzeriaMain;
 import lt.baltictalents.pizzeria.maisto_meniu.DrinkMenuMethods;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 import static lt.baltictalents.pizzeria.read_txt.TxtReader.readMenuTxt;
 
@@ -15,6 +13,11 @@ public class MenuChoices {
     private static DrinkMenuMethods dMM = new DrinkMenuMethods();
     private static List<MenuItem> foodListMenu;
     private static List<MenuItem> drinkListMenu;
+    private static List<MenuItem> customOrder = new ArrayList<>();
+
+    public static List<MenuItem> getCustomOrder() {
+        return customOrder;
+    }
 
     static {
         try {
@@ -55,7 +58,7 @@ public class MenuChoices {
                 break;
             default:
                 System.out.println("Invalid. Input different number");
-//                    caseInput.nextInt();
+//                caseInput.nextInt();
                 mainMenuChoices();
         }
     }
@@ -64,7 +67,7 @@ public class MenuChoices {
         Scanner caseInput = new Scanner(System.in);
         System.out.print("\nWhat would you like to oder?\n");
         System.out.println("\nInput a number");
-        System.out.println("(6 -> Go back )");
+        System.out.println("11 -> Go Back");
         while (!caseInput.hasNextInt()) {
             System.out.println("Invalid. Input different number");
             caseInput.next();
@@ -73,63 +76,70 @@ public class MenuChoices {
         switch (choices) {
             case 1:
                 System.out.println("You ordered : Pizza");
-                PizzeriaMain.main(null);
+                customOrder.add(foodListMenu.get(0));
+                System.out.println(customOrder);
+                productsMenu();
                 break;
             case 2:
                 System.out.println("You ordered : Salad");
-                PizzeriaMain.main(null);
+                customOrder.add(foodListMenu.get(1));
+                System.out.println(customOrder);
+                productsMenu();
                 break;
             case 3:
                 System.out.println("You ordered : Tortilla");
-                PizzeriaMain.main(null);
+                customOrder.add(foodListMenu.get(2));
+                System.out.println(customOrder);
+                productsMenu();
                 break;
             case 4:
                 System.out.println("You ordered : Ice cream");
-                PizzeriaMain.main(null);
+                customOrder.add(foodListMenu.get(3));
+                System.out.println(customOrder);
+                productsMenu();
                 break;
             case 5:
                 System.out.println("You ordered : Soup");
-                PizzeriaMain.main(null);
+                customOrder.add(foodListMenu.get(4));
+                System.out.println(customOrder);
+                productsMenu();
                 break;
             case 6:
                 System.out.println("You ordered : Beer");
-//                dMM.addDrinks("Beer");
-                System.out.println("Added Beer");
-                dMM.addDrinkss("Beer", 1.20);
-//                dMM.addDrinkss("Beer", 1.20).toString();
-//                Set<String> beers = new TreeSet<>();
-//                int beerCount = 0;
-//                if(beerCount == 0){
-//                    beers.add(dMM.addDrinkss("Beer", 1.20).toString());
-//                }
-//                else if(beerCount > 0){
-//                    // ..cia prideti i gala
-//                }
-//                String[] beerTest = beers.toArray(new String[beers.size()]);
-//                System.out.println("beerTest" + Arrays.toString(beerTest));
-//
-//                PizzeriaMain.main(beerTest);
-                PizzeriaMain.main(null);
+                customOrder.add(drinkListMenu.get(0));
+                System.out.println(customOrder);
+                productsMenu();
                 break;
             case 7:
                 System.out.println("You ordered : Root beer");
-                PizzeriaMain.main(null);
+                customOrder.add(drinkListMenu.get(1));
+                System.out.println(customOrder);
+                productsMenu();
                 break;
             case 8:
                 System.out.println("You ordered : Sparkling water");
-                PizzeriaMain.main(null);
+                customOrder.add(drinkListMenu.get(2));
+                System.out.println(customOrder);
+                productsMenu();
                 break;
             case 9:
                 System.out.println("You ordered : Still water");
-                PizzeriaMain.main(null);
+                customOrder.add(drinkListMenu.get(3));
+                System.out.println(customOrder);
+                productsMenu();
                 break;
             case 10:
                 System.out.println("You ordered : Juice");
-                PizzeriaMain.main(null);
+                customOrder.add(drinkListMenu.get(4));
+                System.out.println(customOrder);
+                productsMenu();
                 break;
             case 11:
-                System.out.println("Your order : ");
-                PizzeriaMain.main(null);// cia vietoj null bus perduodamas masyvas su pridetomis reiksmemis
+                System.out.println("Go Back to Menu");
+                mainMenuChoices();
+            case 12:
+                getCustomOrder();
+                System.out.println(getCustomOrder());
             default:
                 System.out.println("Invalid. Input different number");
                 productsMenu();
