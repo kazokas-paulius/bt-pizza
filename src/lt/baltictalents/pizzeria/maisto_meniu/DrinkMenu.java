aa9cd957cffc92d8362521607c38591edac61a64
Paulius Kazokas <BTpauliuskazokas@gmail.com>
2018-08-24 10:34:32 +0300

    Removed unnecessary java classes

--- src/lt/baltictalents/pizzeria/maisto_meniu/DrinkMenu.java
+++
@@ -1,9 +0,0 @@
-package lt.baltictalents.pizzeria.maisto_meniu;
-
-import java.util.Map;
-
-public abstract class DrinkMenu {
-
-//    public abstract String[] addDrinks(String dName, int dPrice);
-    public abstract Map<Double, String> addDrinkss(String dName, double dPrice);
-}
--- src/lt/baltictalents/pizzeria/maisto_meniu/FoodMenu.java
+++
@@ -1,9 +0,0 @@
-package lt.baltictalents.pizzeria.maisto_meniu;
-
-public abstract class FoodMenu {
-
-    public abstract void meals();
-    public abstract void snacks();
-    public abstract void deserts();
-
-}
